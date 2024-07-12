091a371e8c5f10ae3f5e4239eea7b864f45dc5d8
Adolfo Galué <132372341+Adolfo1996@users.noreply.github.com>
2024-07-12 21:49:26 +0000

    Simple counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div class="container text-center reloj" >
+				<div class="row">
+					<div class="col cuadritos">
+						<i className="far fa-clock"></i>
+					</div>
+					<div class="col cuadritos">
+						{props.digito6}
+					</div>
+					<div class="col cuadritos">
+						{props.digito5}
+					</div>
+					<div class="col cuadritos">
+						{props.digito4}
+					</div>
+					<div class="col cuadritos">
+						{props.digito3}
+					</div>
+					<div class="col cuadritos">
+						{props.digito2}
+					</div>
+					<div class="col cuadritos">
+						{props.digito1}
+					</div>
+				</div>
+			</div>
+
+
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -9,5 +9,17 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let segundo = 0
+setInterval(() => {
+    const one = Math.floor((segundo / 1) % 10)
+    const two = Math.floor((segundo / 10) % 10)
+    const three = Math.floor((segundo / 100) % 10)
+    const four = Math.floor((segundo / 1000) % 10)
+    const five = Math.floor((segundo / 10000) % 10)
+    const six = Math.floor((segundo / 100000) % 10)
+    segundo+=1
+    ReactDOM.createRoot(document.getElementById('app')).render(<Home digito1= {one} digito2={two} digito3
+        ={three} digito4={four} digito5={five} digito6={six} />)
+
+}, 1000)
 
